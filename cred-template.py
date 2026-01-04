aee89056f5662465897c690a980740e2268d9274
Vitor Costa <vitortle@gmail.com>
2026-01-04 14:30:24 -0300

    add template and comments

---
+++ cred-template.py
@@ -0,0 +1,15 @@
+DB_CONFIG = {
+    'user': 'db_user',
+    'password': 'db_password',
+    'host': 'ip_address',
+    'port': '3306',
+    'database': 'db_name'
+}
+
+BRICKS_CONFIG = {
+    'token': 'databricks_token',
+    'workspace_url': 'databricks_workspace_url',
+    'catalog': 'catalog_name',
+    'schema': 'schema_name',
+    'volume': 'volume_name'
+}
