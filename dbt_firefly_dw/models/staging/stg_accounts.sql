8cd2988bc3e7ac65912e9c333095dbf4f9ba8b28
Vitor Costa <vitortle@gmail.com>
2026-01-10 16:08:16 -0300

    add schema and sql

--- dbt_firefly_dw/models/staging/stg_accounts.sql
+++ dbt_firefly_dw/models/staging/stg_accounts.sql
@@ -1,5 +1,8 @@
-with source as (
+with s_bz_accounts as (
     select * from {{ source('firefly_raw', 'bz_accounts') }}
+),
+s_bz_account_types as (
+    select * from {{ source('firefly_raw', 'bz_account_types') }}
 ),
 renamed as (
     SELECT
@@ -9,8 +12,8 @@ renamed as (
     a.active AS is_active,
     a.created_at,
     a.updated_at
-FROM firefly.raw.bz_accounts a
- LEFT JOIN firefly.raw.bz_account_types at on 
+FROM s_bz_accounts a
+ LEFT JOIN s_bz_account_types at on 
    a.account_type_id = at.id
 WHERE deleted_at IS NULL
 )
---
+++ dbt_firefly_dw/models/staging/stg_categories.sql
@@ -0,0 +1,16 @@
+with source as (
+    select * from {{ source('firefly_raw', 'bz_accounts') }}
+),
+renamed as (
+    SELECT
+	id AS categories_id,
+	created_at,
+	updated_at,
+	deleted_at,
+	user_id,
+	name AS category_name
+FROM
+	source
+WHERE deleted_at IS null
+)
+select * from renamed
